62dd29d54017f863da9b68129b71a00521b8d344
HyungGi Kim <gudrl201212@gmail.com>
2023-02-05 18:29:36 +0900

    add Webserver

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const contribution = require('./contribution');
+
 const app = express();
 const port = 3000;
+
+app.use('/contribution',contribution);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -9,3 +13,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Sever started on port ${port}`);
 });
+
+module.exports = contribution;
