9a709baf96736a1b5953571359ebe45024772f11
Joonkyo Kim <rootwarp@gmail.com>
2023-02-11 15:35:34 +0900

    Test scala multiplication

--- test/bls.js
+++ test/bls.js
@@ -7,47 +7,103 @@ const G2Point = bls.bls12_381.G2.ProjectivePoint;
 
 const G1Aff = (x, y) => G1Point.fromAffine({ x, y });
 
-describe('bls', function() {
-    it('Fp', function() {
-        // Why do I have to call Fp.create?
-        console.log(G1Point);
-
-        const Fp = bls.bls12_381.CURVE.Fp;
-        const n1 = Fp.create(
-            3971675556538908004130084773503021351583407620890695272226385332452194486153316625183061567093226342405194446632851n
-        );
-        const n2 = Fp.create(
-            1120750640227410374130508113691552487207139112596221955734902008063040284119210871734388578113045163251615428544022n
-        );
-
-        const a = G1Aff(n1, n2);
-        a.assertValidity();
-
-        console.log("A", a);
-    });
-
-    it('mul', function() {
-        console.log('==');
-    });
-});
+// describe('bls', function() {
+//     it('Fp', function() {
+//         // Why do I have to call Fp.create?
+//         console.log(G1Point);
+// 
+//         const Fp = bls.bls12_381.CURVE.Fp;
+//         const n1 = Fp.create(
+//             3971675556538908004130084773503021351583407620890695272226385332452194486153316625183061567093226342405194446632851n
+//         );
+//         const n2 = Fp.create(
+//             1120750640227410374130508113691552487207139112596221955734902008063040284119210871734388578113045163251615428544022n
+//         );
+// 
+//         const a = G1Aff(n1, n2);
+//         a.assertValidity();
+// 
+//         console.log("A", a);
+//     });
+// 
+//     it('mul', function() {
+//         console.log('==');
+//     });
+// });
 
 describe('power-of_tau', function() {
+
+    it('Load contributes', function() {
+        const data = fs.readFileSync('./test/try_contribute.json');
+        const obj = JSON.parse(data);
+        // console.log(obj.contributions);
+
+        obj.contributions.forEach((c, i) => {
+            console.log(i, c.numG1Powers, c.numG2Powers);
+        });
+    });
+
     it('affine transform', function() {
         const data = fs.readFileSync('./test/try_contribute.json');
         const obj = JSON.parse(data);
-        console.log(obj);
+        // console.log(obj.contributions);
 
         // TODO: Affine transform.
         const util = bls.bls12_381.utils;
         const g1Power = "0xaeb984397ba2ff6caac1792ee60910451fb515e02b331398d4d9f6105802fb733361e5b42ce45d4d89ff7af37fe37375";
 
-        const hex = util.hexToBytes(g1Power.substring(2));
-        console.log(hex);
+        const hexStr = g1Power.substring(2);
+        console.log('Hex Str', hexStr);
+
+        const hex = util.hexToBytes(hexStr);
+        console.log('Hex Data', hex);
 
         // No affine transform function.
-        console.log(bls.bls12_381.CURVE.G1.fromBytes(hex));
+        const G1 = bls.bls12_381.CURVE.G1;
+        // const G2 = bls.bls12_381.CURVE.G2;
+        // const Fp12 = bls.bls12_381.CURVE.Fp12;
+
+        const affinePoint = G1.fromBytes(hex);
+        console.log('Affine', affinePoint);
+
+        const prj = G1Point.fromAffine(affinePoint);
+        console.log('Projection', prj);
+        const revert = G1.toBytes(G1Point, prj, true);
+
+        // TODO: Revert. 
+        console.log('Affine to hex', revert);
+        console.log('Affine to hex str1', util.bytesToHex(revert));
 
         // TODO: Create new curve. (Necessary?)
+        // TODO: What is Fr's modulus.
+
+        // ----
+        const prjPointG1 = G1Point.fromAffine(affinePoint);
+        prjPointG1.assertValidity();
+
+        console.log('base1', G1Point.BASE);
+        console.log('proj1', prjPointG1);
+
+        const prjPointG1Mul = prjPointG1.multiply(2n);
+        console.log('proj1 multi', prjPointG1Mul);
+
+        // TODO: Convert to hex
+        console.log('proj1 multi', prjPointG1Mul);
+
+        const mulAffine = prjPointG1Mul.toAffine();
+        console.log('proj1 to Affine', prjPointG1Mul.toAffine());
+
+        console.log('proj1 to Hex', G1.toBytes(G1Point, prjPointG1Mul, true));
+        console.log('proj1 to HexStr', util.bytesToHex(G1.toBytes(G1Point, prjPointG1Mul, true)));
+
+        // ----
+
+        // TODO: Just BigInt multi / mod operation.
+        // const xi = 1n;
+        // const frModulus = 52435875175126190479447740508185965837690552500527637822603658699938581184513n;
+        // console.log('G1', G1);
+        // console.log('G2', G2);
+        // console.log('Fp12', Fp12);
     });
 
     // TODO: Transform all contributes to affine space.
